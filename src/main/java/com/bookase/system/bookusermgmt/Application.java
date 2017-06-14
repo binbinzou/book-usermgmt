268fb188637cb87d5cff6f80f60bfa99fd760317
binbin <binbin@binbin-pc>
2017-06-14 22:46:21 +0800

    add eureka

--- src/main/java/com/bookase/system/bookusermgmt/Application.java
+++ src/main/java/com/bookase/system/bookusermgmt/Application.java
@@ -1,13 +1,18 @@
 package com.bookase.system.bookusermgmt;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
 /**
  * Hello world!
  *
  */
-public class Application 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main( String[] args ) {
+    	SpringApplication.run(Application.class, args);
     }
 }
